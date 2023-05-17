c4852f62e71383cab32254d012b99e66f5797699
Mario8021 <88853720+Mario8021@users.noreply.github.com>
2023-05-17 17:38:36 -0300

    actualizacion deploy koyeb

--- src/main/java/com/portfolio/mario/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mario/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://frontendfinal-55b4e.web.app"})
+@CrossOrigin(origins = "https://localhost:4200")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
