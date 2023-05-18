ce5c520c1ce74deb7c5f0d8624a35602c65b59fa
Mario8021 <88853720+Mario8021@users.noreply.github.com>
2023-05-18 15:30:09 -0300

    actualizacion deploy koyeb

--- src/main/java/com/portfolio/mario/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mario/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "https://frontendfinal-55b4e.web.app")
 
 
 
